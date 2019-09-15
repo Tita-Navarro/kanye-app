0b7480fb878b7a4a762f98643948c74b30365e9d
Martha M. Navarro <tita1089@gmail.com>
2019-09-15 16:24:51 -0500

    adding new branch

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+import Layout from '../components/Layout';
+import Content from '../components/Content';
+import Header from '../components/Header';
+import Card from '../components/Card';
+import Quote from '../components/Quote';
+import {getQuote} from '../lib/quote-api';
+const HELP_TEXT ={
+    fetch : 'With an external fetch'
+};
+const Index =({  quotes  })=> (
+    <Layout>
+        <Content>
+            <Header>Kanye Quotes</Header>
+            {quotes.map(({id, quote})=> (
+                <Card key={id}>
+                    <Quote text={quote} helpText={HELP_TEXT[id]}/>
+                </Card>
+            ))}
+        </Content>
+    </Layout>
+)
+
+Index.getInitialProps = async({req}) =>{
+    const quotes = [
+        {...await getQuote(), id:'fetch'}
+    ]
+    return { quotes }
+};
+
+export default Index;
